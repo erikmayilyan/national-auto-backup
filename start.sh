6446a3ae034440e48ad421a441fd39bde918d20f
Erik <mayilyane9@gmail.com>
2024-06-28 18:38:43 +0200

    again start

--- start.sh
+++ start.sh
@@ -2,10 +2,13 @@
 
 # Start backend server
 cd backend
-npm install
+npm install  # Install backend dependencies
 node server.js &
 
+# Navigate back to parent directory
+cd ..
+
 # Start frontend server
-cd ../frontend
-npm install
-PORT=3001 npm start
+cd frontend
+npm install  # Install frontend dependencies
+PORT=3001 npm start  # Start frontend server with specified PORT
