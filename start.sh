ed720d6ca8a6c971ecdb90447f261671687191ed
Erik <mayilyane9@gmail.com>
2024-06-28 19:19:47 +0200

    again start

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-cd frontend
-npm install
-npm run build
-serve -s build -l 3001 &
-cd ..
 
 # Install backend dependencies and start backend server
 cd backend
 npm install
 node server.js &
+cd ..
+
+# Install frontend dependencies, build frontend, and serve it
+cd frontend
+npm install
+npm run build
+serve -s build -l 3001 &
 cd ..
